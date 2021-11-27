da150292b9ddbe994c2ea97746eb9dc9ef685f7b
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2021-11-27 07:41:37 +0530

    Website Draft version

--- wordcount-project/wordcount/views.py
+++ wordcount-project/wordcount/views.py
@@ -1,6 +1,27 @@
 # from django.http import HttpResponse
+import operator
+
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "home.html")
+    return render(request, "home.html", {"title": "Home"})
+
+
+def count(request):
+    paragraph = request.GET["paragraph"]
+    words = paragraph.split()
+    word_freq = get_frequency(words)
+    wordcount = len(words)
+    print(word_freq)
+    return render(request, "count.html", {"title": "Count",
+                                          "wordcount": wordcount,
+                                          "word_freq": word_freq})
+
+
+def get_frequency(words):
+    word_freq = dict()
+    for word in words:
+        word_freq[word] = word_freq.get(word, 0) + 1
+    sorted_dict = sorted(word_freq.items(), key=operator.itemgetter(1), reverse=True)
+    return sorted_dict
